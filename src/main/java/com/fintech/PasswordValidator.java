26f0e2a65aeb77f70d14a2399008f140d5851f9a
arturflacon <flaconartur@gmail.com>
2026-04-16 13:20:13 -0300

    feat: adiciona validação de letra maiúscula

--- src/main/java/com/fintech/PasswordValidator.java
+++ src/main/java/com/fintech/PasswordValidator.java
@@ -5,6 +5,11 @@ public class PasswordValidator {
     public boolean validate(String password) {
         if (password == null) return false;
         if (password.length() < 8) return false;
+        boolean hasUpper = false;
+        for (char c : password.toCharArray()) {
+            if (Character.isUpperCase(c)) hasUpper = true;
+        }
+        if (!hasUpper) return false;
         return true;
     }
 }
