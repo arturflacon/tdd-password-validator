1ca5a277532c78a9316e29da3209ee6e26d93e56
arturflacon <flaconartur@gmail.com>
2026-04-16 13:21:56 -0300

    feat: adiciona validação de número

--- src/main/java/com/fintech/PasswordValidator.java
+++ src/main/java/com/fintech/PasswordValidator.java
@@ -7,12 +7,15 @@ public class PasswordValidator {
         if (password.length() < 8) return false;
         boolean hasUpper = false;
         boolean hasLower = false;
+        boolean hasDigit = false;
         for (char c : password.toCharArray()) {
             if (Character.isUpperCase(c)) hasUpper = true;
             if (Character.isLowerCase(c)) hasLower = true;
+            if (Character.isDigit(c)) hasDigit = true;
         }
         if (!hasUpper) return false;
         if (!hasLower) return false;
+        if (!hasDigit) return false;
         return true;
     }
 }
