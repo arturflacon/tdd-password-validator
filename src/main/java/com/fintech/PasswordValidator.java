fa8cd70dd71ec25cc60349d9e724c76dcefe3323
arturflacon <flaconartur@gmail.com>
2026-04-16 13:21:27 -0300

    feat: adiciona validação de letra minúscula

--- src/main/java/com/fintech/PasswordValidator.java
+++ src/main/java/com/fintech/PasswordValidator.java
@@ -6,10 +6,13 @@ public class PasswordValidator {
         if (password == null) return false;
         if (password.length() < 8) return false;
         boolean hasUpper = false;
+        boolean hasLower = false;
         for (char c : password.toCharArray()) {
             if (Character.isUpperCase(c)) hasUpper = true;
+            if (Character.isLowerCase(c)) hasLower = true;
         }
         if (!hasUpper) return false;
+        if (!hasLower) return false;
         return true;
     }
 }
