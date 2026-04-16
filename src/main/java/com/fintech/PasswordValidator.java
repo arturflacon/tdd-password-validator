b8344612b600b0480f3c6aa1e9574662cadd2aee
arturflacon <flaconartur@gmail.com>
2026-04-16 13:22:49 -0300

    feat: adiciona validação de caractere especial

--- src/main/java/com/fintech/PasswordValidator.java
+++ src/main/java/com/fintech/PasswordValidator.java
@@ -8,14 +8,18 @@ public class PasswordValidator {
         boolean hasUpper = false;
         boolean hasLower = false;
         boolean hasDigit = false;
+        boolean hasSpecial = false;
+        String specialChars = "!@#$%^&*";
         for (char c : password.toCharArray()) {
             if (Character.isUpperCase(c)) hasUpper = true;
             if (Character.isLowerCase(c)) hasLower = true;
             if (Character.isDigit(c)) hasDigit = true;
+            if (specialChars.indexOf(c) >= 0) hasSpecial = true;
         }
         if (!hasUpper) return false;
         if (!hasLower) return false;
         if (!hasDigit) return false;
+        if (!hasSpecial) return false;
         return true;
     }
 }
